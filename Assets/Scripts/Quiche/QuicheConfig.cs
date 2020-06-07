a6a7d209aa9974347c450c6704ad4c60c9d51fd8
Joseph0304 <joseph0304.drdr@gmail.com>
2020-06-07 15:15:34 +0900

    add quiche config api

--- Assets/Scripts/Quiche/QuicheConfig.cs
+++ Assets/Scripts/Quiche/QuicheConfig.cs
@@ -82,6 +82,38 @@ namespace Quiche
             config = NativeMethods.quiche_config_new(version);
         }
 
+        public int LoadCertChainFromPemFile(string path)
+        {
+            return NativeMethods.quiche_config_load_cert_chain_from_pem_file(
+                config, path);
+        }
+
+        public int LoadPrivKeyFromPemFile(string path)
+        {
+            return NativeMethods.quiche_config_load_priv_key_from_pem_file(
+                config, path);
+        }
+
+        public void VerifyPeer(bool v)
+        {
+            NativeMethods.quiche_config_verify_peer(config, v);
+        }
+
+        public void Grease(bool v)
+        {
+            NativeMethods.quiche_config_grease(config, v);
+        }
+
+        public void LogKeys()
+        {
+            NativeMethods.quiche_config_log_keys(config);
+        }
+
+        public void EnableEarlyData()
+        {
+            NativeMethods.quiche_config_enable_early_data(config);
+        }
+
         public void SetApplicationProtos(byte[] protos)
         {
             NativeMethods.quiche_config_set_application_protos(
@@ -105,7 +137,14 @@ namespace Quiche
 
         public void SetInitialMaxStreamDataBidiLocal(ulong maxData)
         {
-            NativeMethods.quiche_config_set_initial_max_stream_data_bidi_local(config, maxData);
+            NativeMethods.quiche_config_set_initial_max_stream_data_bidi_local(
+                config, maxData);
+        }
+
+        public void SetInitialMaxStreamDataBidiRemote(ulong maxData)
+        {
+            NativeMethods.quiche_config_set_initial_max_stream_data_bidi_remote(
+                config, maxData);
         }
 
         public void SetInitialMaxStreamDataUni(ulong maxData)
@@ -123,14 +162,19 @@ namespace Quiche
             NativeMethods.quiche_config_set_initial_max_streams_uni(config, maxStreams);
         }
 
+        public void SetAckDelayExponent(ulong v)
+        {
+            NativeMethods.quiche_config_set_ack_delay_exponent(config, v);
+        }
+
+        public void SetMaxAckDelay(ulong v)
+        {
+            NativeMethods.quiche_config_set_max_ack_delay(config, v);
+        }
+
         public void SetDisableActiveMigration(bool v)
         {
             NativeMethods.quiche_config_set_disable_active_migration(config, v);
-        }
-
-        public void VerifyPeer(bool v)
-        {
-            NativeMethods.quiche_config_verify_peer(config, v);
         }
 
         public void Dispose()
